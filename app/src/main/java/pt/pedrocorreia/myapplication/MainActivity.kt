2ee0fc8c2c0beaef7b24c73bb042e87ef0508083
Pedro Correia <pedrogrcorreia@gmail.com>
2022-11-10 15:48:05 +0000

    get location using the map tool on emulator

--- app/src/main/java/pt/pedrocorreia/myapplication/MainActivity.kt
+++ app/src/main/java/pt/pedrocorreia/myapplication/MainActivity.kt
@@ -1,21 +1,164 @@
 package pt.pedrocorreia.myapplication
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationListenerCompat
+import pt.pedrocorreia.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private val locationManager : LocationManager by lazy {
+    private val locationManager: LocationManager by lazy {
         getSystemService(LOCATION_SERVICE) as LocationManager
     }
 
     private var coarseLocationPermission = false
     private var fineLocationPermission = false
+    private var backgroundLocationPermission = false
+    private var locationEnabled = false
+
+    private val nullString : String? = null
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        verifyPermissions()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun verifyPermissions() {
+        coarseLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        fineLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            backgroundLocationPermission = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        } else
+            backgroundLocationPermission = coarseLocationPermission || fineLocationPermission
+
+        if (!coarseLocationPermission && !fineLocationPermission) {
+            basicPermissionsAuthorization.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        } else
+            verifyBackgroundPermission()
+    }
+
+    private val basicPermissionsAuthorization = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { results ->
+        coarseLocationPermission = results[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false
+        fineLocationPermission = results[Manifest.permission.ACCESS_FINE_LOCATION] ?: false
+        startLocationUpdates()
+        verifyBackgroundPermission()
+    }
+
+    private fun verifyBackgroundPermission() {
+        if (!(coarseLocationPermission || fineLocationPermission))
+            return
+
+        if (!backgroundLocationPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            ) {
+                val dlg = AlertDialog.Builder(this)
+                    .setTitle("Background Location")
+                    .setMessage(
+                        "This application needs your permission to use location while in the background.\n" +
+                                "Please choose the correct option in the following screen" +
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+                                    " (\"${packageManager.backgroundPermissionOptionLabel}\")."
+                                else
+                                    "."
+                    )
+                    .setPositiveButton("Ok") { _, _ ->
+                        backgroundPermissionAuthorization.launch(
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                        )
+                    }
+                    .create()
+                dlg.show()
+            }
+        }
+    }
+
+    private val backgroundPermissionAuthorization = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { result ->
+        backgroundLocationPermission = result
+        Toast.makeText(this,"Background location enabled: $result",Toast.LENGTH_LONG).show()
+    }
+
+    private var currentLocation = Location(nullString)
+        set(value) {
+            field = value
+            binding.tvLat.text = String.format("%10.5f", value.latitude)
+            binding.tvLon.text = String.format("%10.5f", value.longitude)
+        }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        if (locationEnabled || !(coarseLocationPermission || fineLocationPermission))
+            return
+
+        currentLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER)
+            ?: Location(nullString).apply {
+                latitude = 40.1925
+                longitude = -8.4128
+            }
+
+        locationManager.requestLocationUpdates(
+            if (fineLocationPermission) LocationManager.GPS_PROVIDER else LocationManager.NETWORK_PROVIDER,
+            1000,
+            100f,
+            locationListener
+        )
+        locationEnabled = true
+    }
+
+    private val locationListener = LocationListenerCompat {
+            location -> currentLocation = location
+    }
+
+    private fun stopLocationUpdates() {
+        if (!locationEnabled)
+            return
+        locationManager.removeUpdates(locationListener)
+        locationEnabled = false
     }
 }
